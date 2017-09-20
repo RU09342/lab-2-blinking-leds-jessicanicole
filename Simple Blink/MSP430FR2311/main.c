8a02bfcc6b4f82f46d4872dd0801245edcff55f1
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-09-19 21:01:49 -0400

    initial code

---
+++ Simple Blink/MSP430FR2311/main.c
@@ -0,0 +1,19 @@
+#include <msp430.h>
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   //stop watchdog timer
+
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
+
+    PM5CTL0 &= ~LOCKLPM5;
+
+    int count = 0;
+
+    while(1)                     //created infinite loop
+    {
+      P1OUT ^= 0x01;              // toggles LED on and off
+      for (count=0;count<15000;count++);    // delay between the toggle
+    }
+}
