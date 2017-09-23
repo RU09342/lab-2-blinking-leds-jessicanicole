d80fecc286a6cfdaa610fbc1eec87101abc88c14
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-09-23 18:00:36 -0400

    added code for G2 button blink with extra comments

--- Button Blink/MSP430G2553/main.c
+++ Button Blink/MSP430G2553/main.c
@@ -10,15 +10,15 @@ WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
 P1OUT &= ~LED1;                         // Clear P1.0 output latch for a defined power-on state
 P1DIR = LED1;                          // Set P1.0 to output direction
 
-P1DIR &= ~BUTTON;
+P1DIR &= ~BUTTON; // sets button to input direction
 P1REN ^= BUTTON; //Enables a puller-Resistor on the button-pin
 P1OUT ^= BUTTON; //Writes a "1" to the portpin, telling the resistor to pullup
 
 while(1)
 {
     if (!(BUTTON & P1IN))
-       P1OUT |= LED1;  // Blink Led
+       P1OUT |= LED1;  // Blink LED when button is pressed
     else
-       P1OUT &= ~LED1;
+       P1OUT &= ~LED1; //LED off when button is not pressed
 }
 }
