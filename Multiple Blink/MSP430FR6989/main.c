0f38f462cecd4ad3688c4578aed21f897e1ec465
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-09-23 17:55:11 -0400

    added code for 6989 multi blink

---
+++ Multiple Blink/MSP430FR6989/main.c
@@ -0,0 +1,34 @@
+#include <msp430.h>
+
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   //stop watchdog timer
+
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
+
+    P9OUT &= ~BIT7;                         // Clear P2.0 output latch for a defined power-on state
+    P9DIR |= BIT7;                          // Set P2.0 to output direction
+    PM5CTL0 &= ~LOCKLPM5;
+
+    int count = 0;
+    int count1 = 0;
+
+    // loop allows 2 LED to blink continuously
+    while(1)
+    {
+      if (count >= 15000)
+      {
+          P1OUT ^= BIT0;
+          count = 0;
+      }
+      if (count1 >= 30000)
+      {
+          P9OUT ^= BIT7;
+          count1 = 0;
+      }
+
+      count++;
+      count1++;
+    }
+}
