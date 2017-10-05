5ce78efa06cbffb47af4bfad6e057942f722a5ec
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-10-05 14:11:26 -0400

    Updated code doc for 6989

--- Button Blink/MSP430FR6989/main.c
+++ Button Blink/MSP430FR6989/main.c
@@ -1,25 +1,29 @@
+//Button Blink on the MSP430FR6989
+//Jessica Wozniak
+//Created: 09/13/17
+//Last updated: 9/23/17
+
 #include <msp430FR6989.h>
 
-#define BUTTON BIT1
-#define LED1 BIT0
+#define BUTTON BIT1  //defines Button as BIT1
+#define LED1 BIT0   //deinfes LED as BIT0
 
 void main(void)
 {
 WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
 
-P1OUT &= ~LED1;                         // Clear P1.0 output latch for a defined power-on state
-P1DIR = LED1;                          // Set P1.0 to output direction
-
-P1DIR &= ~BUTTON;
+P1OUT &= ~LED1;                         // Clear LED bit
+P1DIR = LED1;                          // Sets LED as output
+P1DIR &= ~BUTTON; //sets BUTTON as input
 P1REN ^= BUTTON; //Enables a puller-Resistor on the button-pin
 P1OUT ^= BUTTON; //Writes a "1" to the portpin, telling the resistor to pullup
 PM5CTL0 &= ~LOCKLPM5;
 
 while(1)
 {
-    if (!(BUTTON & P1IN))
-       P1OUT |= LED1;  // Blink Led
+    if (!(BUTTON & P1IN)) // if button is not pressed
+       P1OUT |= LED1;  // LED on
     else
-       P1OUT &= ~LED1;
+       P1OUT &= ~LED1; //LED off, if button is not pressed
 }
 }
