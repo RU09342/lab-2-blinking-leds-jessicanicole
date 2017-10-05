2945800907808d36e267849fed53d21535222e77
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-10-05 14:12:34 -0400

    Updated code doc for G2

--- Button Blink/MSP430G2553/main.c
+++ Button Blink/MSP430G2553/main.c
@@ -1,24 +1,31 @@
-#include <msp430.h>
+//Button Blink on the MSP430G2553
+//Jessica Wozniak
+//Created: 09/13/17
+//Last updated: 9/23/17
 
-#define BUTTON BIT3
-#define LED1 BIT0
+#include <msp430g2553.h>
+
+#define BUTTON BIT3  //defines BUTTON as BIT3
+#define LED1 BIT0    //Defines LED1 as BIT0
 
 void main(void)
 {
 WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
 
-P1OUT &= ~LED1;                         // Clear P1.0 output latch for a defined power-on state
-P1DIR = LED1;                          // Set P1.0 to output direction
+P1OUT &= ~LED1;                         // sets initial state of LED1 (P1.0)
+P1DIR = LED1;                          // Set LED as an output
 
-P1DIR &= ~BUTTON; // sets button to input direction
+P1DIR &= ~BUTTON; // sets button as an input
 P1REN ^= BUTTON; //Enables a puller-Resistor on the button-pin
 P1OUT ^= BUTTON; //Writes a "1" to the portpin, telling the resistor to pullup
 
-while(1)
+while(1)  //creates infinite loop
 {
-    if (!(BUTTON & P1IN))
-       P1OUT |= LED1;  // Blink LED when button is pressed
+    if (!(BUTTON & P1IN))  //if BUTTON is not pressed
+    {
+       P1OUT |= LED1;  // LED on
+    }
     else
-       P1OUT &= ~LED1; //LED off when button is not pressed
+       P1OUT &= ~LED1; //LED off when button is pressed
 }
 }
